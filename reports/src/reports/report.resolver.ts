fdc4e70aaddcb0c41b15ace38821b5cf90f469b4
Andre Baltieri <andrebaltieri@hotmail.com>
2019-05-13 14:30:03 -0300

    Removido código desnecessario

--- reports/src/reports/report.resolver.ts
+++ reports/src/reports/report.resolver.ts
@@ -1,11 +1,7 @@
-import { NotFoundException } from '@nestjs/common';
-import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
-import { PubSub } from 'apollo-server-express';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Product } from './models/product.model';
 import { ReportService } from './report.service';
 import { ProductArgs } from './dtos/product-args.dto';
-
-// const pubSub = new PubSub();
 
 @Resolver(of => Product)
 export class ReportsResolver {
@@ -14,9 +10,6 @@ export class ReportsResolver {
     @Query(returns => Product)
     async product(@Args('id') id: string): Promise<Product> {
         const product = await this.service.findOneById(id);
-        if (!product) {
-            throw new NotFoundException(id);
-        }
         return product;
     }
 
